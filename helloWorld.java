9a8169882e547472ea8c8d9ea985daa270376fa6
ericallione <ericallione@gmail.com>
2017-10-14 19:01:26 -0700

    helloWorld

---
+++ helloWorld.java
@@ -0,0 +1,11 @@
+/* helloWorld 2017Oct14 14:51 */
+
+public class helloWorld
+{
+    public static void main(String[] args)
+    {
+        {
+            System.out.println("hello world");    
+        }
+    }
+}
