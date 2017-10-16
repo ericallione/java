af88eebf21648164e80fa1d837e5b9fe5edf368d
ericallione <ericallione@gmail.com>
2017-10-15 18:53:53 -0700

    commit from Mac VIM git. Changed to helloJava.

---
+++ helloJava.java
@@ -0,0 +1,11 @@
+/* helloWorld 2017Oct14 14:51 */
+
+public class helloJava
+{
+    public static void main(String[] args)
+    {
+        {
+            System.out.println("hello java");    
+        }
+    }
+}
